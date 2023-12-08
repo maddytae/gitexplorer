d898e9f889ef1c181d62be1ea9acad7cca2965d9
Madhukar Jha <madhukar.k.jha@gmail.com>
2023-12-07 21:12:35 -0500

    saving work

--- app.py
+++ app.py
@@ -54,27 +54,32 @@ def main():
         # Generate a unique session identifier
         if 'session_id' not in session:
             session['session_id'] = str(uuid.uuid4())
-            session_dir = os.path.join(st.repo_store, f"{session['session_id']}")
-            session['session_dir'] = session_dir
-            if not os.path.exists(session_dir):
-                os.makedirs(session_dir, exist_ok=True)
+
+        session_dir = os.path.join(st.repo_store, f"{session['session_id']}")
+        main_diff_dir=os.path.join(session_dir,"main_diff_path")
+        print(main_diff_dir)
+        session['session_dir'] = session_dir
+        session['main_diff_dir'] = main_diff_dir
+        if not os.path.exists(session_dir):
+            os.makedirs(session_dir, exist_ok=True)
+        if not os.path.exists(main_diff_dir):
+            os.makedirs(main_diff_dir, exist_ok=True)
 
         if 'sshAddress' in request.form:
             sshAddress = request.form.get("sshAddress")
+            session["sshAddress"] = sshAddress
             if sshAddress:
                 repo_name = sshAddress.split('/')[-1].replace('.git', '')
                 # Set paths for repo and diff
-                repo_path = os.path.join(session.get('session_dir', ''), repo_name)
-                diff_path = os.path.join(session.get('session_dir', ''), 'diff')
+                repo_path = os.path.join(session_dir, repo_name)
+                diff_path = os.path.join(session_dir, 'diff')
 
                 # Store paths in session
-                # session['user_dir'] = user_dir
                 session['repo_path'] = repo_path
                 session['diff_path'] = diff_path
 
                 try:
-                    ut.repo_cloning(sshAddress, session.get('repo_path', ''), st.repo_size_limit)
-                    session["sshAddress"] = sshAddress
+                    ut.repo_cloning(sshAddress, repo_path, st.repo_size_limit)
                     return redirect(url_for("repo", repo_name=repo_name))
                 except ut.SizeLimitExceededError as e:
                     error_message = "Repo exceeded size limit."
@@ -96,9 +101,9 @@ def main():
             filename2='code2.txt'
 
             try:
-                with open(os.path.join(session_dir, filename1), 'w') as file:
+                with open(os.path.join(main_diff_dir, filename1), 'w') as file:
                     file.write(code1)
-                with open(os.path.join(session_dir, filename2), 'w') as file:
+                with open(os.path.join(main_diff_dir, filename2), 'w') as file:
                     file.write(code2)
             except Exception as e:
                 error_message = f"An error occurred while saving files: {e}"
@@ -114,8 +119,8 @@ def repo(repo_name):
 
     repo_path=session.get('repo_path', '')
     diff_path=session.get('diff_path','')
+    os.makedirs(repo_path,exist_ok=True)
     os.makedirs(diff_path,exist_ok=True)
-    # ut.clear_directory(diff_path)
 
 
     branches = ut.get_git_branches(repo_path) if repo_path else []
@@ -150,13 +155,8 @@ def repo(repo_name):
         if selected_branch2:
             commits2 = ut.get_commits(repo_path, selected_branch2,selected_author2)
 
-
-
         if selected_commit1 and selected_commit2:
             filePaths = ut.get_modified_files(repo_path,selected_commit1,selected_commit2)
-            
-
-
 
             output_path1 = os.path.join(diff_path, 'folder_diff.html')
             output_path2 = os.path.join(diff_path, 'folder_diff_modifications_only.html')
